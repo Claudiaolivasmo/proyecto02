f2579b6f5041468bed1f5cf761f722bd57a760f8
Minor <greivin5324@mail.com>
2025-11-20 10:20:40 -0600

    - Implementé la estructura base del menú principal. - Creé las clases para gestión de pantallas y navegación. - Integré pygame_menu como librería para construir el menú.

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-'''
-===================================
-Proyecto 2 - Introducción a la programación
+import pygame
+import pygame_menu
 
-Mainor Martinez
-Claudia Olivas
+pygame.init()
 
-===================================
+class LaberintoGame:
+    def __init__(self):
+        self.WIDTH, self.HEIGHT = 800, 600
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Escape del Laberinto")
 
-'''
+        self.player_name = ""
+        self.menu = MainMenu(self) #pasamos la app al menu
+        self.game_loop = GameLoop(self)
+
+    def run(self):
+        "Inicia el menú principal"
+        self.menu.show()
+
+    def set_player_name(self, name):
+        self.player_name = name
+        print(f"Nombre del jugador establecido a: {self.player_name}")
+
+
+
+# Clase para el menú principal
+class MainMenu:
+    def __init__(self, app):
+        self.app = app  #Referencia a la aplicación principal
+        self.menu = pygame_menu.Menu(
+            "Escapa del Laberinto",
+            app.WIDTH,
+            app.HEIGHT,
+            theme=pygame_menu.themes.THEME_BLUE
+        )
+
+        #campo de texto
+        self.menu.add.text_input("Nombre: ", onchange=self.app.set_player_name)
+
+class GameLoop:
+    pass  # Aquí iría la lógica del juego

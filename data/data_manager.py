06f6ab1c4542de415a97c46e7ea21e4d2feb3fb7
Minor <greivin5324@mail.com>
2025-11-21 14:06:21 -0600

    Estructura de puntajes en el archivo JSON

--- data/data_manager.py
+++ data/data_manager.py
@@ -2,6 +2,9 @@ import json
 import time
 import os
 
+SCORES_PATH = os.path.join('data','assets','scores.json')
 
+
+#TODO: Completar la clase DataManager para manejar las puntuaciones y otros datos
 class DataManager:
     pass
